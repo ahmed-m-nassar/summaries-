c913398fc4a456c6426651643a2750106ae2b203
Ahmed Mohamed Nassar <ahmed.nassar.cmp@gmail.com>
2024-05-29 11:48:38 +0300

    adding llm

---
+++ llm/projects/code_gen/main.py
@@ -0,0 +1,40 @@
+
+import google.generativeai as genai
+genai.configure(api_key="AIzaSyANxTuizKCseQrc6w0gZpQxYJW2Ozz9ins")
+
+# Set up the model
+generation_config = {
+  "temperature": 1,
+  "top_p": 0.95,
+  "top_k": 0,
+  "max_output_tokens": 8192,
+}
+
+safety_settings = [
+  {
+    "category": "HARM_CATEGORY_HARASSMENT",
+    "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+  },
+  {
+    "category": "HARM_CATEGORY_HATE_SPEECH",
+    "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+  },
+  {
+    "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
+    "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+  },
+  {
+    "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
+    "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+  },
+]
+
+model = genai.GenerativeModel(model_name="gemini-1.5-pro-latest",
+                              generation_config=generation_config,
+                              safety_settings=safety_settings)
+
+convo = model.start_chat(history=[
+])
+
+convo.send_message("write a short poem")
+print(convo.last.text)
